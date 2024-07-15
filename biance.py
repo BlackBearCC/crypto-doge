af8867ec78d734d69ef0f256ed9b416488fca6cd
leobigbig <leozy0819@gmail.com>
2024-07-15 15:19:07 +0800

    添加：数据分析及回测时间范围选择

--- biance.py
+++ biance.py
@@ -1,11 +1,12 @@
 import asyncio
+import datetime
 import json
 from pathlib import Path
 
 import ccxt.pro
 import numpy as np
 import pytz
-from backtrader import Cerebro
+from backtrader import Cerebro, TimeFrame
 from backtrader.feeds import PandasData
 
 from ta.momentum import RSIIndicator
@@ -80,9 +81,9 @@ def read_and_combine_csv(directory):
 class MultiTimeFrameRSIStrategy(bt.Strategy):
     params = (
         ('rsi_window', 14),
-        ('buy_threshold', 30),
-        ('sell_threshold', 72),
-        ('cooldown_period', 120),
+        ('buy_threshold', 22),
+        ('sell_threshold', 60),
+        ('cooldown_period', 20),
     )
 
     def __init__(self):
@@ -103,23 +104,32 @@ class MultiTimeFrameRSIStrategy(bt.Strategy):
         if self.sell_cooldown > 0:
             self.sell_cooldown -= 1
 
+        amount_to_invest = 100
+        current_price = self.datas[0].close[0]
+        size = amount_to_invest / current_price
+        rsi_average = (self.rsi_5m[0] + self.rsi_15m[0] + self.rsi_30m[0]) / 3
+
         # 检查所有RSI指标是否低于买入阈值
         if (self.rsi_5m < self.params.buy_threshold and
             self.rsi_15m < self.params.buy_threshold and
             self.rsi_30m < self.params.buy_threshold and
             self.buy_cooldown == 0):
-            self.buy(size=100, name="Buy")
+            if self.position.size < 0:
+                self.close()
+            self.buy(size=size)
             self.buy_cooldown = self.params.cooldown_period
-            self.log('BUY CREATE, %.2f' % self.datas[0].close[0])
+            self.log('BUY , %.2f, SIZE: %.2f' % (self.datas[0].close[0], size))
 
         # 检查所有RSI指标是否高于卖出阈值
         elif (self.rsi_5m > self.params.sell_threshold and
               self.rsi_15m > self.params.sell_threshold and
               self.rsi_30m > self.params.sell_threshold and
               self.sell_cooldown == 0):
-            self.sell(size=100, name="Sell")
+            if self.position.size>0:
+                self.close()
+            self.sell(size=size)
             self.sell_cooldown = self.params.cooldown_period
-            self.log('SELL CREATE, %.2f' % self.datas[0].close[0])
+            self.log('SELL , %.2f, SIZE: %.2f' % (self.datas[0].close[0], size))
 
 
 
@@ -128,11 +138,12 @@ class MultiTimeFrameRSIStrategy(bt.Strategy):
         if isinstance(dt, float):  # Backtrader stores datetimes as floats internally
             dt = bt.num2date(dt)  # Convert to datetime object
 
-        # Convert UTC to China time (UTC+8)
-        china_tz = pytz.timezone('Asia/Shanghai')
-        dt = dt.replace(tzinfo=pytz.utc).astimezone(china_tz)
-
-        print('%s, %s' % (dt.isoformat(), txt))
+        # # Convert UTC to China time (UTC+8)
+        # china_tz = pytz.timezone('Asia/Shanghai')
+        # dt = dt.replace(tzinfo=pytz.utc).astimezone(china_tz)
+        dt = pd.Timestamp(dt).tz_localize('UTC').tz_convert('Asia/Shanghai')
+        formatted_dt = dt.strftime('%Y-%m-%d %H:%M:%S')
+        print('%s, %s' % (formatted_dt, txt))
 
 import backtrader.analyzers as btanalyzers
 if __name__ == '__main__':
@@ -148,6 +159,13 @@ if __name__ == '__main__':
     for timeframe, dir_path in directories.items():
         combined_df = read_and_combine_csv(dir_path)
         data = bt.feeds.PandasData(dataname=combined_df)
+        # 设置时间范围
+        todate = datetime.datetime.now()
+        fromdate = todate - datetime.timedelta(days=30)
+
+        data = bt.feeds.PandasData(dataname=combined_df, fromdate=fromdate, todate=todate)
+        # data = bt.feeds.PandasData(dataname=combined_df)
+
         if timeframe == '5m':
             data.plotinfo.plot = True  # Only the 5-minute data is plotted on the main plot
         else:
@@ -156,24 +174,62 @@ if __name__ == '__main__':
         cerebro.adddata(data)
 
     cerebro.broker.setcash(5000)  # 初始资金设为5000
-    # 添加您的数据和策略
+
+    # 添加数据和策略
     cerebro.addstrategy(MultiTimeFrameRSIStrategy)
-
-    cerebro.addanalyzer(btanalyzers.SharpeRatio, _name='sharpe_ratio')
-    cerebro.addanalyzer(btanalyzers.DrawDown, _name='drawdown')
+    cerebro.addanalyzer(btanalyzers.SharpeRatio, _name='sharpe_ratio', timeframe=TimeFrame.Minutes, compression=60,
+                        annualize=True)
+    cerebro.addanalyzer(btanalyzers.AnnualReturn, _name='annual_returns')
+    cerebro.addanalyzer(btanalyzers.Returns, _name='returns', fund=False)
+    cerebro.addanalyzer(btanalyzers.TimeReturn, _name='time_return', fund=False)
+    cerebro.addanalyzer(btanalyzers.DrawDown, _name='drawdown', fund=False)
     cerebro.addanalyzer(btanalyzers.TradeAnalyzer, _name='trade_analyzer')
-
 
     # 运行策略
     results = cerebro.run()
     strat = results[0]
 
-
-    print('夏普比率:', strat.analyzers.sharpe_ratio.get_analysis())
-    print('最大回撤:', strat.analyzers.drawdown.get_analysis()['max']['drawdown'])
-    trade_analysis = strat.analyzers.trade_analyzer.get_analysis()
-    print('总交易数:', trade_analysis)
-
+    # 获取分析器的结果
+    sharpe_ratio = strat.analyzers.sharpe_ratio.get_analysis()
+    annual_returns = strat.analyzers.annual_returns.get_analysis()
+    returns = strat.analyzers.returns.get_analysis()
+    time_return = strat.analyzers.time_return.get_analysis()
+    drawdown = strat.analyzers.drawdown.get_analysis()
+    trade_analyzer = strat.analyzers.trade_analyzer.get_analysis()
+    trade_info = {
+        '总交易数': trade_analyzer.total.total,
+        '未平仓交易数': trade_analyzer.total.open,
+        '已平仓交易数': trade_analyzer.total.closed,
+        '当前连赢': trade_analyzer.streak.won.current,
+        '最长连赢': trade_analyzer.streak.won.longest,
+        '当前连输': trade_analyzer.streak.lost.current,
+        '最长连输': trade_analyzer.streak.lost.longest,
+        '总毛收益': trade_analyzer.pnl.gross.total,
+        '平均毛收益': trade_analyzer.pnl.gross.average,
+        '总净收益': trade_analyzer.pnl.net.total,
+        '平均净收益': trade_analyzer.pnl.net.average,
+        '获胜交易数': trade_analyzer.won.total,
+        '总获胜收益': trade_analyzer.won.pnl.total,
+        '平均获胜收益': trade_analyzer.won.pnl.average,
+        '最大获胜收益': trade_analyzer.won.pnl.max,
+        '失败交易数': trade_analyzer.lost.total,
+        '总失败收益': trade_analyzer.lost.pnl.total,
+        '平均失败收益': trade_analyzer.lost.pnl.average,
+        '最大失败收益': trade_analyzer.lost.pnl.max,
+        '多头交易数': trade_analyzer.long.total,
+        '空头交易数': trade_analyzer.short.total,
+        '多头总毛收益': trade_analyzer.long.pnl.total,
+        '多头平均收益': trade_analyzer.long.pnl.average,
+        '空头总毛收益': trade_analyzer.short.pnl.total,
+        '空头平均收益': trade_analyzer.short.pnl.average,
+        '胜率': f"{trade_analyzer.won.total / trade_analyzer.total.closed * 100 if trade_analyzer.total.closed > 0 else 0}%"
+    }
+    print(f"交易信息:\n{trade_info}")
+    print(f"夏普比率: {sharpe_ratio['sharperatio']}")
+    print(f"年化收益率: {annual_returns}")
+    print(f"总收益率: {returns}")
+    # print(f"时间收益: {time_return}")
+    print(f"最大回撤: {drawdown}")
 
     # 设置图形参数并绘图
     import matplotlib.pyplot as plt
